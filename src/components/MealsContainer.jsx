6f2227e4d508517aef4eba3c7fa8960e3b1d0750
MoeElItani <mhmd98itani@gmail.com>
2022-12-29 21:29:53 +0200

    bug fixes

--- src/components/MealsContainer.jsx
+++ src/components/MealsContainer.jsx
@@ -6,9 +6,11 @@ const MealsContainer = ({ meals }) => {
          id='recipes'
          className='homeBg flex flex-wrap justify-around w-full my-[60px]'
       >
-         {meals.map((meal) => (
-            <MealCard meal={meal} key={meal.idMeal} />
-         ))}
+         {!meals || meals === null || meals === undefined
+            ? <p className='text-center text-red-400 font-semibold text-[1.15rem]'>Meal Not Found! <br /> Please Try Searching for Another Meal </p>
+            : meals.map((meal) => (
+                 <MealCard meal={meal} key={meal.idMeal} />
+              ))}
       </div>
    )
 }

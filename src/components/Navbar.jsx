773debede2ed6d84e4622e683965a2201da86176
MoeElItani <mhmd98itani@gmail.com>
2022-12-29 21:56:08 +0200

    minor design changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
    return (
-      <nav className='flex justify-between mx-8 my-5'>
-         <a
-            href='/'
-            className='text-teal-900 flex-auto font-semibold'
-         >
+      <nav className='absolute p-2 md:mx-8 md:p-4 top-0 left-0 z-50 flex items-center justify-center md:justify-start w-full md:w-[97%] m-[auto]'>
+         <h1 className='text-white font-semibold text-xl shadow-2xl shadow-[#1DE9B6]'>
             RecipesDB
-         </a>
+         </h1>
       </nav>
    )
 }
